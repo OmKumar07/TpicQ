d1e9598fcced50f548167afb5b987c54d0b2e94a
OmKumar07 <omkumarishere@gmail.com>
2025-09-11 15:36:53 +0530

    update frontend to use deployed backend url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://tpicq.onrender.com";
 
 // Predefined topics list
 const AVAILABLE_TOPICS = [
